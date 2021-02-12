41f0d44c1b319d4f54fa2254248854be7f305e39
Pivithuru Jayakody <piveedj@gmail.com>
2021-02-12 13:57:42 +0530

    Register Microservice (for Testing)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
